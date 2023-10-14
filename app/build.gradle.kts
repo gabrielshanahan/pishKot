b1fbd3352c71dd58ded747f31d6e37baad33536e
Gabriel Shanahan <GabrielShanahan@gmail.com>
2023-10-14 17:26:03 +0200

    Added Procfile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,14 @@
+import io.gitlab.arturbosch.detekt.Detekt
+import io.gitlab.arturbosch.detekt.DetektCreateBaselineTask
+
+val ktorVersion: String by project
+val logbackVersion: String by project
+val slackVersion: String by project
+
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.9.10"
+    id("org.jmailen.kotlinter") version "4.0.0"
+    id("io.gitlab.arturbosch.detekt").version("1.23.1")
     application
 }
 
@@ -8,11 +17,17 @@ repositories {
 }
 
 dependencies {
-    implementation("com.google.guava:guava:32.1.1-jre")
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-gson:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
 
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.3")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+
+    implementation("com.slack.api:slack-api-client:$slackVersion")
+    implementation("com.slack.api:slack-api-model-kotlin-extension:$slackVersion")
+    implementation("com.slack.api:slack-api-client-kotlin-extension:$slackVersion")
+
+    testImplementation("io.ktor:ktor-server-tests:$ktorVersion")
 }
 
 java {
@@ -27,4 +42,18 @@ application {
 
 tasks.named<Test>("test") {
     useJUnitPlatform()
+}
+
+detekt {
+    buildUponDefaultConfig = true
+    allRules = false
+    config.setFrom("$rootDir/config/detekt/detekt.yml")
+    baseline = file("$rootDir/config/detekt/baseline.xml")
+}
+
+tasks.withType<Detekt>().configureEach {
+    jvmTarget = JavaLanguageVersion.of(20).toString()
+}
+tasks.withType<DetektCreateBaselineTask>().configureEach {
+    jvmTarget = JavaLanguageVersion.of(20).toString()
 }
